eab452dc9b5ad571ba61a9c1e1a9264d0e7ad8a2
听着情歌流泪 <develop@lingtal.com>
2017-12-25 18:08:27 +0800

    完善rem处理

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -3,6 +3,10 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {}{{#isMobile}},
+    "postcss-px2rem" : {
+      remUnit:100,
+      remPrecision: 5
+    }{{/isMobile}}
   }
 }
